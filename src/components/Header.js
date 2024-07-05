ae799b9c2d3f974d1a5061bd771c2a3a700592ff
DuyCR7 <anhduy0317@gmail.com>
2024-07-05 20:47:02 +0700

    count users

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,12 @@ import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import {useSelector} from "react-redux";
 
 const Header = (props) => {
+
+    const listUsers = useSelector((state) => state.user.listUsers);
+
     return (
         <>
             <Navbar expand="lg" className="bg-body-tertiary">
@@ -12,18 +16,11 @@ const Header = (props) => {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
-                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">
-                                    Another action
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">
-                                    Separated link
-                                </NavDropdown.Item>
+                            <NavDropdown title={`${listUsers.length} Users`} id="basic-nav-dropdown">
+                                {listUsers && listUsers.length > 0 &&
+                                listUsers.map((user, index) => (
+                                    <NavDropdown.Item key={index} href="#">{user.email}</NavDropdown.Item>
+                                ))}
                             </NavDropdown>
                         </Nav>
                     </Navbar.Collapse>
