e2a3ca5e48dc85301e0a50e0a0257ec24be2d180
DuyCR7 <anhduy0317@gmail.com>
2024-07-05 20:07:14 +0700

    create user with redux

--- src/components/FormAddNew.js
+++ src/components/FormAddNew.js
@@ -1,27 +1,61 @@
 import Container from "react-bootstrap/Container";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import {useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {createUser} from "../action/actions";
 
 const FormAddNew = (props) => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [username, setUsername] = useState("");
+
+    const dispatch = useDispatch();
+    const isCreating = useSelector((state) => state.user.isCreating);
+
+    const handleCreateUser = () => {
+        dispatch(createUser(email, password, username));
+        setEmail("");
+        setPassword("");
+        setUsername("");
+    }
+
     return (
         <>
             <Container>
                 <Form>
                     <Form.Group className="mb-3">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Control
+                            type="email"
+                            placeholder="Enter email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}/>
                     </Form.Group>
 
                     <Form.Group className="mb-3">
                         <Form.Label>Username</Form.Label>
-                        <Form.Control type="text" placeholder="Enter username" />
+                        <Form.Control
+                            type="text"
+                            placeholder="Enter username"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}/>
                     </Form.Group>
 
                     <Form.Group className="mb-3">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control
+                            type="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}/>
                     </Form.Group>
-                    <Button variant="primary" type="submit">
+                    <Button
+                        variant="primary"
+                        onClick={() => handleCreateUser()}
+                        disabled={isCreating}
+                    >
                         Create
                     </Button>
                 </Form>
